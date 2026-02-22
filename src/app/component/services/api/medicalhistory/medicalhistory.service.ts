0dc8e84a24079f9a8d4277c50d85d3e43023d219
emcb-ekanayaka <ekanayakabdr@gmail.com>
2026-02-22 16:49:54 +0530

    Medical History Service File Changed

--- src/app/component/services/api/medicalhistory/medicalhistory.service.ts
+++ src/app/component/services/api/medicalhistory/medicalhistory.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
@@ -8,39 +8,51 @@ import { Observable } from 'rxjs';
 export class MedicalHistoryService {
 
   private baseUrl: string = 'http://localhost:8010/api/v1/medicalhistory';
+  
   constructor(
     private http: HttpClient
   ) { }
 
+  // Helper method to get headers with token
+  private getHeaders(): HttpHeaders {
+    const token = localStorage.getItem('token'); // or sessionStorage
+    return new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
+    });
+  }
 
   createMedicalHistory(medicalHistory: any, type: any): Observable<any> {
-    alert('Medical History saved successfully!');
+    const headers = this.getHeaders();
+    
     if (type == 'Add') {
-      return this.http.post(this.baseUrl, medicalHistory);
+      return this.http.post(this.baseUrl, medicalHistory, { headers });
     } else {
-      return this.http.put(this.baseUrl + "/" + medicalHistory.id, medicalHistory);
+      return this.http.put(this.baseUrl + "/" + medicalHistory.id, medicalHistory, { headers });
     }
-
   }
 
   getAllMedicalHistories(page: number = 0, size: number = 10): Observable<any> {
-      let params = new HttpParams()
-        .set('page', page.toString())
-        .set('size', size.toString());
-  
-      return this.http.get<any>(this.baseUrl, { params });
-    }
-    
+    const headers = this.getHeaders();
+    let params = new HttpParams()
+      .set('page', page.toString())
+      .set('size', size.toString());
+
+    return this.http.get<any>(this.baseUrl, { params, headers });
+  }
 
   GetAllMedicalHistory(): Observable<any> {
-    return this.http.get(this.baseUrl);
+    const headers = this.getHeaders();
+    return this.http.get(this.baseUrl, { headers });
   }
 
   GetMedicalHistoryById(ID: any): Observable<any> {
-    return this.http.get(this.baseUrl + "/" + ID);
+    const headers = this.getHeaders();
+    return this.http.get(this.baseUrl + "/" + ID, { headers });
   }
 
   DeleteMedicalHistoryById(ID: any): Observable<any> {
-    return this.http.delete(this.baseUrl + "/" + ID)
+    const headers = this.getHeaders();
+    return this.http.delete(this.baseUrl + "/" + ID, { headers });
   }
 }
